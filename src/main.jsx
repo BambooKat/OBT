4a7c83aca576df4944c0018bbaa20511d8ee7fe1
MakieKojima <makie.kojima@gmail.com>
2026-07-13 12:32:19 +0200

    fix login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- importa questo
 import App from './App.jsx'
 import './styles/theme.css'
+import './App.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* <-- aggiungi wrapper */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
